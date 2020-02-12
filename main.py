f4b9e63dbf6cad33af699eb50b48f6a78d15452a
George-Derganov <gderganov@yandex.ru>
2020-02-12 21:55:43 +0400

    add CCC

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ class Search(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.info.setVisible(False)
-        self.info_2.setVisible(False)
         self.btn_search.clicked.connect(self.search_word)
 
         if os.path.exists("words.db") is False:
@@ -23,40 +21,50 @@ class Search(QMainWindow, Ui_MainWindow):
 
     def search_word(self):
         word = self.word.toPlainText()
-        print(word)
+
         if len(word) != 0:
             info1 = self.cur_one.execute("""select * from words1 where translate like '%{}%'""".format(word)).fetchall()
             info2 = self.cur_one.execute("""select * from words2 where translate like '%{}%'""".format(word)).fetchall()
-            self.view(info1, self.tableWidget_1, self.info)
-            self.view(info2, self.tableWidget_2, self.info_2)
+            info3 = self.cur_one.execute("""select * from words3 where translate like '%{}%'""".format(word)).fetchall()
         else:
             info1 = self.cur_one.execute("""select * from words1""").fetchall()
             info2 = self.cur_one.execute("""select * from words2""").fetchall()
-            self.view(info1, self.tableWidget_1, self.info)
-            self.view(info2, self.tableWidget_2, self.info_2)
+            info3 = self.cur_one.execute("""select * from words3""").fetchall()
 
-    def view(self, info, table, wrong):
+        self.view(info1, self.tableWidget_1)
+        self.view(info2, self.tableWidget_2)
+        self.view(info3, self.tableWidget_3)
+
+    def view(self, info, table):
         if len(info) > 0:
             table.clear()
-
             table.setRowCount(len(info))
             table.setColumnCount(len(info[0]))
 
             for i, elem in enumerate(info):
                 for j, val in enumerate(elem):
                     table.setItem(i, j, QTableWidgetItem(str(val)))
-            wrong.setVisible(False)
+
+            if table == self.tableWidget_1:
+                self.info_1.setText('ССС (найдено {})'.format(len(info)))
+
+            if table == self.tableWidget_2:
+                self.info_2.setText('СДРЯ (найдено {})'.format(len(info)))
+
+            if table == self.tableWidget_3:
+                self.info_3.setText('ЭССЯ (найдено {})'.format(len(info)))
+
         else:
             table.clear()
-            wrong.setVisible(True)
-            wrong.setText('Ничего не найдено!')
 
-    def create_table(self, conn, table):
-        try:
-            c = conn.cursor()
-            c.execute(table)
-        except Error as e:
-            print(e)
+            if table == self.tableWidget_1:
+                self.info_1.setText('ССС (не найдено)')
+
+            if table == self.tableWidget_2:
+                self.info_2.setText('СДРЯ (не найдено)')
+
+            if table == self.tableWidget_3:
+                self.info_3.setText('ЭССЯ (не найдено)')
 
     def create_bd(self):
         database = r"words.db"
@@ -68,33 +76,60 @@ class Search(QMainWindow, Ui_MainWindow):
                                         word      STRING,
                                         translate STRING);"""
 
+        words3_table = """CREATE TABLE words3 (
+                                word      STRING,
+                                translate STRING);"""
+
         conn = sqlite3.connect(database)
         self.create_table(conn, words1_table)
         self.create_table(conn, words2_table)
+        self.create_table(conn, words3_table)
+
+    def create_table(self, conn, table):
+        try:
+            c = conn.cursor()
+            c.execute(table)
+        except Error as e:
+            print(e)
 
     def add_data(self):
 
         #  words1
+        data = open("data/ССС.txt", mode='r', encoding='utf-8').readlines()
+        con = sqlite3.connect("words.db")
+        cur = con.cursor()
+        for i in data:
+            a, b = i.split('\t')
+            b = b.split('\n')[0][:-1]
+            print(a, b)
+            cur.execute("""INSERT INTO words1(word) VALUES('{}')""".format(a)).fetchall()
+            cur.execute("""UPDATE words1 SET translate = '{}' where word = '{}'""".format(b, a)).fetchall()
+            con.commit()
+        con.close()
+
+        #  words2
         data = open("data/СДРЯ.txt", mode='r', encoding='utf-8').readlines()
         con = sqlite3.connect("words.db")
         cur = con.cursor()
         for i in data:
             a, b = i.split('\t')
             b = b.split('\n')[0][:-1]
-            cur.execute("""INSERT INTO words1(word) VALUES('{}')""".format(a)).fetchall()
-            cur.execute("""UPDATE words1 SET translate = '{}' where word = '{}'""".format(b, a)).fetchall()
+            print(a, b)
+            cur.execute("""INSERT INTO words2(word) VALUES('{}')""".format(a)).fetchall()
+            cur.execute("""UPDATE words2 SET translate = '{}' where word = '{}'""".format(b, a)).fetchall()
             con.commit()
         con.close()
 
-        # words2
+        # words3
         data = open("data/ЭССЯ.txt", mode='r', encoding='utf-8').readlines()
         con = sqlite3.connect("words.db")
         cur = con.cursor()
         for i in data:
             a, b = i.split('\t')
             b = b.split('\n')[0][:-1]
-            cur.execute("""INSERT INTO words2(word) VALUES("{}")""".format(a)).fetchall()
-            cur.execute('''UPDATE words2 SET translate = "{}" where word = "{}"'''.format(b, a)).fetchall()
+            print(a, b)
+            cur.execute("""INSERT INTO words3(word) VALUES("{}")""".format(a)).fetchall()
+            cur.execute('''UPDATE words3 SET translate = "{}" where word = "{}"'''.format(b, a)).fetchall()
             con.commit()
         con.close()
 
